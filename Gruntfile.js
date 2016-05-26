724fc60dc6621fcdda3cc95e04107bca22f2dda6
Anton Wilhelm <timaschew@gmail.com>
2016-05-26 18:43:57 +0200

    what a beautiful code :)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,15 @@
-module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON( 'package.json' ),
-		release: {
-			options: {
-				github: { 
-					repo: 'hoxton-one/deepstream.io-cache-and-storage-connector-template',
-					usernameVar: 'GITHUB_USERNAME',
-					passwordVar: 'GITHUB_PASSWORD'
-				}
-			}
-		}
-	});
-
-	grunt.loadNpmTasks( 'grunt-release' );
-};
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    release: {
+      options: {
+        github: {
+          repo: 'hoxton-one/deepstream.io-cache-and-storage-connector-template',
+          usernameVar: 'GITHUB_USERNAME',
+          passwordVar: 'GITHUB_PASSWORD'
+        }
+      }
+    }
+  })
+  grunt.loadNpmTasks('grunt-release')
+}
