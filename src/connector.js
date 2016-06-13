25dc4c1344ec5df9364180f37813403461035bdf
Yasser Fadl <yasser.fadl@gmail.com>
2016-06-13 19:07:25 +0200

    Updating to karma

--- src/connector.js
+++ src/connector.js
@@ -1,108 +1,109 @@
-var events = require( 'events' ),
-	util = require( 'util' ),
-	Memcached = require( 'memcached' ),
-	pckg = require( '../package.json' );
+'use strict'
+
+const events = require('events')
+const Memcached = require('memcached')
+const pckg = require('../package.json')
 
 /**
  * This class connects deepstream.io to a memcached cache, using the
  * memcached library (https://www.npmjs.com/package/memcached).
- * 
+ *
  * Please consult https://www.npmjs.com/package/memcached for details
  * on the serverLocation and memcachedOptions setting
- * 
+ *
  * lifetime is the default lifetime for objects in seconds (defaults to 1000)
- * 
+ *
  * @author Wolfram Hempel
  * @copyright 2015 Hoxton One Ltd.
- * 
+ *
  * @param {Object} options { serverLocation: <mixed>, [lifetime]: <Number>, [memcachedOptions]: <Object> }
  *
  * @constructor
  */
-var Connector = function( options ) {
-	this.isReady = false;
-	this.name = pckg.name;
-	this.version = pckg.version;
-	this._options = options;
-	this._options.lifetime = options.lifetime || 1000;
-	
-	if( !this._options.serverLocation ) {
-		throw new Error( 'Missing parameter \'serverLocation\' for memcached connector' );
-	}
-	
-	this._client = new Memcached( this._options.serverLocation, this._options.memcachedOptions || {} );
-	this._client.on( 'failure', this.emit.bind( this, 'error' ) );
-	
-	process.nextTick( this._ready.bind( this ) );
-};
+class Connector extends events.EventEmitter {
+  constructor(options) {
+    super(options)
+    this.isReady = false
+    this.name = pckg.name
+    this.version = pckg.version
+    this._options = options
+    this._options.lifetime = options.lifetime || 1000
 
-util.inherits( Connector, events.EventEmitter );
+    if (!this._options.serverLocation) {
+      throw new Error('Missing parameter \'serverLocation\' for memcached connector')
+    }
 
-/**
- * Writes a value to the cache.
- *
- * @param {String}   key
- * @param {Object}   value
- * @param {Function} callback Should be called with null for successful set operations or with an error message string
- *
- * @private
- * @returns {void}
- */
-Connector.prototype.set = function( key, value, callback ) {
-	this._client.set( key, value, this._options.lifetime, this._onResponse.bind( this, callback ) );
-};
+    this._client = new Memcached(this._options.serverLocation, this._options.memcachedOptions || {})
+    this._client.on('failure', this.emit.bind(this, 'error'))
 
-/**
- * Retrieves a value from the cache
- *
- * @param {String}   key
- * @param {Function} callback Will be called with null and the stored object
- *                            for successful operations or with an error message string
- *
- * @private
- * @returns {void}
- */
-Connector.prototype.get = function( key, callback ) {
-	this._client.get( key, function( err, value ){
-		if( err ) {
-			callback( err );
-			return;
-		}
-		
-		if( value === undefined ) {
-			callback( null, null );
-			return;
-		}
-		
-		callback( null, value );
-	});
-};
+    process.nextTick(this._ready.bind(this))
+  }
 
-/**
- * Deletes an entry from the cache.
- *
- * @param   {String}   key
- * @param   {Function} callback Will be called with null for successful deletions or with
- *                     an error message string
- *
- * @private
- * @returns {void}
- */
-Connector.prototype.delete = function( key, callback ) {
-	this._client.del( key, this._onResponse.bind( this, callback ) );
-};
+  /**
+   * Writes a value to the cache.
+   *
+   * @param {String}   key
+   * @param {Object}   value
+   * @param {Function} callback Should be called with null for successful set operations or with an error message string
+   *
+   * @private
+   * @returns {void}
+   */
+  set(key, value, callback) {
+    this._client.set(key, value, this._options.lifetime, this._onResponse.bind(this, callback))
+  }
 
-Connector.prototype._ready = function() {
-	this.isReady = true;
-	this.emit( 'ready' );
-};
+  /**
+   * Retrieves a value from the cache
+   *
+   * @param {String}   key
+   * @param {Function} callback Will be called with null and the stored object
+   *                            for successful operations or with an error message string
+   *
+   * @private
+   * @returns {void}
+   */
+  get(key, callback) {
+    this._client.get(key, (err, value) => {
+      if (err) {
+        callback(err)
+        return
+      }
 
-Connector.prototype._onResponse = function( callback, error ) {
-	if( error ) {
-		callback( error );
-	} else {
-		callback( null );
-	}
-};
+      if (value === undefined) {
+        callback(null, null)
+        return
+      }
+      callback(null, value)
+    })
+  }
 
-module.exports = Connector;
+  /**
+   * Deletes an entry from the cache.
+   *
+   * @param   {String}   key
+   * @param   {Function} callback Will be called with null for successful deletions or with
+   *                     an error message string
+   *
+   * @private
+   * @returns {void}
+   */
+  delete(key, callback) {
+    this._client.del(key, this._onResponse.bind(this, callback))
+  }
+
+  _ready() {
+    this.isReady = true
+    this.emit('ready')
+  }
+
+  _onResponse(callback, error) {
+    if (error) {
+      callback(error)
+    } else {
+      callback(null)
+    }
+  }
+}
+
+module.exports = Connector
--- test/cache-connectorSpec.js
+++ test/cache-connectorSpec.js
@@ -1,66 +1,71 @@
-/* global describe, expect, it, jasmine */
-var CacheConnector = require( '../src/connector' ),
-	EventEmitter = require( 'events' ).EventEmitter,
-	settings = { serverLocation: process.env.MEMCACHED_URL || 'localhost:11211' },
-	MESSAGE_TIME = 20;
+/* global describe, it, beforeEach */
+'use strict'
 
-describe( 'the message connector has the correct structure', function(){
-	var cacheConnector;
+const expect = require('chai').expect
+const CacheConnector = require('../src/connector')
+const EventEmitter = require('events').EventEmitter
+const settings = { serverLocation: process.env.MEMCACHED_URL || 'localhost:11211' }
 
-	it( 'throws an error if required connection parameters are missing', function(){
-		expect(function(){ new CacheConnector( 'gibberish' ); }).toThrow();
-	});
+describe('the message connector has the correct structure', () => {
+  let cacheConnector
 
-	it( 'creates the cacheConnector', function( done ){
-		cacheConnector = new CacheConnector( settings );
-		expect( cacheConnector.isReady ).toBe( false );
-		cacheConnector.on( 'ready', done );
-	});
+  beforeEach(() => {
+    cacheConnector = new CacheConnector(settings)
+  })
 
-	it( 'implements the cache/storage connector interface', function() {
-	    expect( typeof cacheConnector.name ).toBe( 'string' );
-	    expect( typeof cacheConnector.version ).toBe( 'string' );
-	    expect( typeof cacheConnector.get ).toBe( 'function' );
-	    expect( typeof cacheConnector.set ).toBe( 'function' );
-	    expect( typeof cacheConnector.delete ).toBe( 'function' );
-	    expect( cacheConnector instanceof EventEmitter ).toBe( true );
-	});
+  it('throws an error if required connection parameters are missing', () => {
+    expect(() => { new CacheConnector('gibberish') }).to.throw()
+  })
 
-	it( 'retrieves a non existing value', function( done ){
-		cacheConnector.get( 'someValue', function( error, value ){
-			expect( error ).toBe( null );
-			expect( value ).toBe( null );
-			done();
-		});
-	});
+  it('creates the cacheConnector', done => {
+    expect(cacheConnector.isReady).to.equal(false)
+    cacheConnector.on('ready', done)
+  })
 
-	it( 'sets a value', function( done ){
-		cacheConnector.set( 'someValue', { firstname: 'Wolfram' }, function( error ){
-			expect( error ).toBe( null );
-			done();
-		});
-	});
+  it('implements the cache/storage connector interface', () => {
+    expect(cacheConnector.name).be.a('string')
+    expect(cacheConnector.version).be.a('string')
+    expect(cacheConnector.get).be.a('function')
+    expect(cacheConnector.set).be.a('function')
+    expect(cacheConnector.delete).be.a('function')
+    expect(cacheConnector instanceof EventEmitter).equal(true)
+  })
 
-	it( 'retrieves an existing value', function( done ){
-		cacheConnector.get( 'someValue', function( error, value ){
-			expect( error ).toBe( null );
-			expect( value ).toEqual( { firstname: 'Wolfram' } );
-			done();
-		});
-	});
+  it('retrieves a non existing value', done => {
+    cacheConnector.get('someValue', (error, value) => {
+      expect(error).to.equal(null)
+      expect(value).to.equal(null)
+      done()
+    })
+  })
 
-	it( 'deletes a value', function( done ){
-		cacheConnector.delete( 'someValue', function( error ){
-			expect( error ).toBe( null );
-			done();
-		});
-	});
+  it('sets a value', done => {
+    cacheConnector.set('someValue', { firstname: 'Wolfram' }, error => {
+      expect(error).to.equal(null)
+      done()
+    })
+  })
 
-	it( 'Can\'t retrieve a deleted value', function( done ){
-		cacheConnector.get( 'someValue', function( error, value ){
-			expect( error ).toBe( null );
-			expect( value ).toBe( null );
-			done();
-		});
-	});
-});
+  it('retrieves an existing value', done => {
+    cacheConnector.get('someValue', (error, value) => {
+      expect(error).to.equal(null)
+      expect(value).to.eql({ firstname: 'Wolfram' })
+      done()
+    })
+  })
+
+  it('deletes a value', done => {
+    cacheConnector.delete('someValue', error => {
+      expect(error).to.equal(null)
+      done()
+    })
+  })
+
+  it('Can\'t retrieve a deleted value', done => {
+    cacheConnector.get('someValue', (error, value) => {
+      expect(error).to.equal(null)
+      expect(value).to.equal(null)
+      done()
+    })
+  })
+})
